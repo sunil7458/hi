fdc75d90b02c4874c41e31505476b7dffbf13602
sunil7458 <115408245+sunil7458@users.noreply.github.com>
2022-10-23 22:21:51 +0530

    jjj

--- src/coponent/User.js
+++ src/coponent/User.js
@@ -7,7 +7,7 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import { useQuery } from "react-query";
 import axios from "axios";
 

0627fc8393b1dbe981ccc826b92b77fb33c8556b
sunil7458 <115408245+sunil7458@users.noreply.github.com>
2022-10-23 22:32:55 +0530

    o

--- src/coponent/Edit.js
+++ src/coponent/Edit.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-import React, { useState } from "react";
-import { useMutation } from "react-query";
+import React, { useState } from "react"
 import { useNavigate } from 'react-router-dom'
 import Nav from "../Nav";
 
